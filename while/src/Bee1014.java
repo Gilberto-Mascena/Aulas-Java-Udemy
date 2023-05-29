4f4936cb8bdb73b640ef1e3a3359ba8ad80f201b
Gilberto Silva <gilbertosilva@iMac-Pro-de-Gilberto-Silva.local>
2023-05-29 16:18:58 -0300

    Add new files While

---
+++ while/src/Bee1014.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bee1014 {
+    
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        double y = sc.nextDouble();
+
+        double media = (double)x / y;
+
+        System.out.printf("%.3f km/l%n", media);
+
+        sc.close();
+    }
+}
